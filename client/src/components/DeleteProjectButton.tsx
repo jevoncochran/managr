7db524aee08aac651153bc772c2055ce7dbd3f47
Jevon Cochran <jevoncochran@Jevons-MacBook-Pro.local>
2022-12-03 13:14:24 -0600

    first commit

---
+++ client/src/components/DeleteProjectButton.tsx
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router-dom";
+import { FaTrash } from "react-icons/fa";
+import { GET_PROJECTS } from "../queries/projectQueries";
+import { useMutation } from "@apollo/client";
+import { DELETE_PROJECT } from "../mutations/projectMutations";
+
+interface DeleteProjectButtonProps {
+  projectId: string;
+}
+
+export default function DeleteProjectButton({
+  projectId,
+}: DeleteProjectButtonProps) {
+  const navigate = useNavigate();
+
+  const [deleteProject] = useMutation(DELETE_PROJECT, {
+    variables: { id: projectId },
+    update(cache, { data: { deleteProject } }) {
+      const { projects } = cache.readQuery({ query: GET_PROJECTS });
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: {
+          projects: projects.filter(
+            (project) => project.id !== deleteProject.id
+          ),
+        },
+      });
+    },
+    onCompleted: () => navigate("/"),
+  });
+  return (
+    <div className="d-flex mt-5 ms-auto">
+      <button className="btn btn-danger m-2" onClick={() => deleteProject()}>
+        <FaTrash className="icon" /> Delete Project
+      </button>
+    </div>
+  );
+}
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import graphqlLogo from "../assets/graphql_logo.png";
+
+export default function Header() {
+  return (
+    <nav className="navbar bg-light mb-4 p-0">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <div className="d-flex">
+            <img src={graphqlLogo} alt="graphql logo" className="mr-2" />
+            <div>Project MGMT</div>
+          </div>
+        </a>
+      </div>
+    </nav>
+  );
+}
